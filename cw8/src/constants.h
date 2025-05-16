9f1c109cfebbf70e9bd1ef2f2241236e2b00448a
Eg-Nickname <djkubax@op.pl>
2025-05-17 00:54:41 +0200

    [Task 8] queue name fix

--- cw8/src/constants.h
+++ cw8/src/constants.h
@@ -1,7 +1,7 @@
 #ifndef QUEUE_CONSTANS
 #define QUEUE_CONSTANS
 
-#define SERVER_QUEUE_NAME "server_calculator_queue"
+#define SERVER_QUEUE_NAME "/server_calculator_queue"
 #define MAX_SERVER_MSG_SIZE 512
 #define MAX_CLIENT_SERVER_MSG_SIZE 255
 
